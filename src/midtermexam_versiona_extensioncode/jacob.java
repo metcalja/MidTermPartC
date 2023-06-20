0effff1da8c6708ade472de9c5e29dc0fd27fd1e
Jacob Metcalfe <metcalja@sheridancollege.ca>
2023-06-20 09:45:52 -0400

    commiting jacob.java

---
+++ src/midtermexam_versiona_extensioncode/jacob.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author yacob
+ */
+public class jacob {
+    //made a branch
+    //added this class to it
+    //commiting and pushing now
+}
